2be9a0b847347ade5fe2b38617bde2a27019706e
Ezra Singh <ezra@liontech.nyc>
2020-03-17 15:18:26 -0400

    Bootstrapped simple demo

--- src/app/main.js
+++ src/app/main.js
@@ -1,3 +1,19 @@
-require('./style.sass');
+require('./style.sass')
 
-console.log("Hello World");
+const appRoot = document.querySelector('.app')
+const toggleBtn = document.querySelector('#toggle-btn')
+
+function toggleTheme(){
+	switch(appRoot.getAttribute('theme')){
+		case 'light':
+			appRoot.setAttribute('theme', 'dark')
+			break
+		case 'dark':
+			appRoot.setAttribute('theme', 'light')
+			break
+		default:
+			return
+	}
+}
+
+toggleBtn.addEventListener('click', toggleTheme)
